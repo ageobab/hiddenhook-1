73b36f04acbf6a478bd28fb4a4c73897e33e94db
ageobab <agbabiki@gmail.com>
2014-04-17 14:48:24 -0400

    secured the portal section

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Hiddenhook::Application.routes.draw do
-
+  devise_for :users, skip: [:registrations], controllers: {sessions: 'sessions'}
+  devise_scope :user do
+    delete "logout" => "devise/sessions#destroy", as: "logout"
+  end
   mount RailsAdmin::Engine => '/portal', as: 'rails_admin'
-
-  devise_for :users, skip: [:registrations]
 
   root to: "static#index"
   resources :charges
