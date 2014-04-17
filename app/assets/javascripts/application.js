31809506d15c4fbcc3d509a3b41d6048c64fb6b4
ageobab <agbabiki@gmail.com>
2014-04-17 14:16:57 -0400

    scroll tight in line, javascript changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,13 @@
 //= require bootstrap.min
 //= require retina
 //= require jquery.easing.1.3
-//= require smoothscroll
 //= require_self
+
+$(function() {
+  $('a.smoothScroll').click(function() {
+    $('html, body').animate({
+      scrollTop: $(this.hash).position().top
+    }, 1000);
+    return false;
+  });
+});
